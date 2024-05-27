aa9d637b2a26a56894409e47b3038a3d19340e4f
TrickBySaqib <160585814+TrickBySaqib@users.noreply.github.com>
2024-05-27 19:24:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/badmunda011/PBXMUSIC",
+    "https://github.com/TrickBySaqib/TrickVipNew",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
